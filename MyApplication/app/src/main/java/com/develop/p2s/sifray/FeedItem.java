07f27115534937e878980315f473c07131572a2f
Rinov <rinov982011@gmail.com>
2016-07-02 14:02:03 +0700

    dududu

---
+++ MyApplication/app/src/main/java/com/develop/p2s/sifray/FeedItem.java
@@ -0,0 +1,7 @@
+package com.develop.p2s.sifray;
+
+/**
+ * Created by Rinov on 7/2/2016.
+ */
+public class FeedItem {
+}
---
+++ MyApplication/app/src/main/java/com/develop/p2s/sifray/ListRow.java
@@ -0,0 +1,13 @@
+package com.develop.p2s.sifray;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListRow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_row);
+    }
+}
